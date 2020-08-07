352d55af59d82aab2c2eee3bfbbe384f8dc50e5a
skprbot <skprbot@skpr.io>
2020-08-07 03:44:55 +0000

    Release v0.6.4-kimtest8

--- Formula/skpr.rb
+++ Formula/skpr.rb
@@ -1,9 +1,9 @@
 class Skpr < Formula
   desc "CLI for the Skpr Hosting Platform"
   homepage "https://www.skpr.io"
-  url "https://github.com/skpr/cli/releases/download/v0.6.4-kimtest5/skpr_darwin_amd64.tgz"
-  version "v0.6.4-kimtest5"
-  sha256 "466b53baf05f5d4fb7080f494b9c583cbb71182ab341e5422d202e2dc37c063a"
+  url "https://github.com/skpr/cli/releases/download/v0.6.4-kimtest8/skpr_darwin_amd64.tgz"
+  version "v0.6.4-kimtest8"
+  sha256 "3327854d6c96db804135108d02e4c8f484065d8263c510defb992ec245df49c2"
 
   def install
     bin.install "skpr"
