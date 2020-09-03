43a7350c1cfeaf0504e0da9786d348adeba1b649
skprbot <skprbot@skpr.io>
2020-09-03 06:00:42 +0000

    Release v0.7.0

--- Formula/skpr.rb
+++ Formula/skpr.rb
@@ -1,9 +1,9 @@
 class Skpr < Formula
   desc "CLI for the Skpr Hosting Platform"
   homepage "https://www.skpr.io"
-  url "https://github.com/skpr/cli/releases/download/v0.6.5/skpr_darwin_amd64.tgz"
-  version "v0.6.5"
-  sha256 "71b773e7a5e039dc6b5d40256b9e82061a8f283f67ce26b5a57a36fcc07c852a"
+  url "https://github.com/skpr/cli/releases/download/v0.7.0/skpr_darwin_amd64.tgz"
+  version "v0.7.0"
+  sha256 "9976c11c188747bb643cfae0b8952bef0c89822f93950dd2842a093cc39de7c9"
 
   def install
     bin.install "skpr"
