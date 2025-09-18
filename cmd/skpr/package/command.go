aef62c3070fe912cc42941a1a3a807277ea32328
Nick Schuch <nick@previousnext.com.au>
2025-09-18 10:22:13 +1000

    Adds --build-args (#14)

--- cmd/skpr/package/command.go
+++ cmd/skpr/package/command.go
@@ -44,6 +44,7 @@ func NewCommand() *cobra.Command {
 	cmd.Flags().BoolVar(&command.Params.NoPush, "no-push", command.Params.NoPush, "Do not push the image to the registry.")
 	cmd.Flags().BoolVar(&command.PrintManifest, "print-manifest", command.PrintManifest, "Print the manifest to stdout.")
 	cmd.Flags().StringVar(&command.PackageDir, "dir", ".skpr/package", "The location of the package directory.")
+	cmd.Flags().StringSliceVar(&command.BuildArgs, "build-arg", []string{}, "Additional build arguments.")
 	cmd.Flags().BoolVar(&command.Debug, "debug", command.Debug, "Enable debug output.")
 
 	return cmd
