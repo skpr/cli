15f80b0595e8362a36fdc5090e0a1e27ed34314e
Nick Schuch <nick@previousnext.com.au>
2025-10-23 10:52:46 +1000

    Adds environment variable for discovery: SKPR_PROJECT_DIRECTORY (#29)
    * Adds environment variable for discovery: SKPR_PROJECT_DIRECTORY
    
    * Update errors and add checks

--- internal/client/utils/config.go
+++ internal/client/utils/config.go
@@ -1,20 +1,45 @@
 package utils
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 )
 
 const (
+	// Directory name for Skpr project configuration
 	ProjectConfigDir = ".skpr"
+	// Environment variable for declaring the project directory
+	EnvProjectDirectory = "SKPR_PROJECT_DIRECTORY"
 )
 
 // FindSkprConfigDir walks up directories until it finds one containing `.skpr`.
 // Returns the path to that directory, or an error if not found.
-func FindSkprConfigDir(startDir string) string {
-	dir, err := filepath.Abs(startDir)
+func FindSkprConfigDir() (string, error) {
+	// Check for environment variable override
+	envProjectDirectory := os.Getenv(EnvProjectDirectory)
+	if envProjectDirectory != "" {
+		exists, err := checkDirectory(envProjectDirectory)
+		if err != nil {
+			return "", fmt.Errorf("failed to check if directory from %s exists: %w", EnvProjectDirectory, err)
+		}
+
+		if !exists {
+			return "", fmt.Errorf("directory from %s does not exist or is not a directory: %s", EnvProjectDirectory, envProjectDirectory)
+		}
+
+		return envProjectDirectory, nil
+	}
+
+	// Not environment variable was provided. Let's use the current directory.
+	curDir, err := os.Getwd()
 	if err != nil {
-		return ""
+		return "", err
+	}
+
+	dir, err := filepath.Abs(curDir)
+	if err != nil {
+		return "", err
 	}
 
 	for {
@@ -22,15 +47,29 @@ func FindSkprConfigDir(startDir string) string {
 		info, err := os.Stat(skprPath)
 		if err == nil && info.IsDir() {
 			// Found the directory containing `.skpr`
-			return filepath.Join(dir, ProjectConfigDir)
+			return filepath.Join(dir, ProjectConfigDir), nil
 		}
 
 		parent := filepath.Dir(dir)
 		if parent == dir {
 			// Reached filesystem root
-			return ""
+			return "", fmt.Errorf("walked to the root directory and unable to find a %s config directory", ProjectConfigDir)
 		}
 
 		dir = parent
 	}
+}
+
+// Checks if a directory at the given path exists.
+func checkDirectory(path string) (bool, error) {
+	info, err := os.Stat(path)
+	if err == nil {
+		return info.IsDir(), nil
+	}
+
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+
+	return false, err
 }
