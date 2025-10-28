0d804c119a7c93088d59e9267a0467967501cbf8
Nathan ter Bogt <nterbogt@gmail.com>
2025-10-28 17:08:19 +1300

    Updating command to request database name (#33)

--- cmd/skpr/mysql/backup/create/command.go
+++ cmd/skpr/mysql/backup/create/command.go
@@ -11,10 +11,10 @@ var (
 
 	cmdExample = `
   # Create a MySQL backup of dev.
-  skpr mysql backup create dev
+  skpr mysql backup create dev default
 
   # Create and wait for a MySQL backup.
-  skpr mysql backup create dev --wait`
+  skpr mysql backup create dev default --wait`
 )
 
 // NewCommand creates a new cobra.Command for 'create' sub command
@@ -22,14 +22,15 @@ func NewCommand() *cobra.Command {
 	command := v1create.Command{}
 
 	cmd := &cobra.Command{
-		Use:                   "create <environment>",
-		Args:                  cobra.ExactArgs(1),
+		Use:                   "create <environment> <name>",
+		Args:                  cobra.ExactArgs(2),
 		DisableFlagsInUseLine: true,
 		Short:                 "Create a MySQL backup",
 		Long:                  cmdLong,
 		Example:               cmdExample,
 		RunE: func(cmd *cobra.Command, args []string) error {
 			command.Environment = args[0]
+			command.DatabaseName = args[1]
 			return command.Run(cmd.Context())
 		},
 	}
