b7ae950a37b54cd4b914d446e7b14fa296aa5e5a
Nick Schuch <nick@previousnext.com.au>
2026-05-07 01:26:45 +1000

    New region: ap-southeast-6

--- internal/aws/const.go
+++ internal/aws/const.go
@@ -3,5 +3,8 @@ package aws
 // RegionApSouthEast2 is an AWS region.
 const RegionApSouthEast2 = "ap-southeast-2"
 
+// RegionApSouthEast6 is an AWS region.
+const RegionApSouthEast6 = "ap-southeast-6"
+
 // RegionUsEast1 is an AWS region.
 const RegionUsEast1 = "us-east-1"
