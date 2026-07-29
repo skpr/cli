c0680753cebdbd047a4f11e5ee931920526e1c64
Nathan ter Bogt <nterbogt@gmail.com>
2026-07-29 14:33:32 +1200

    Get the project from the config

--- internal/command/console/command.go
+++ internal/command/console/command.go
@@ -8,6 +8,7 @@ import (
 	"github.com/skratchdot/open-golang/open"
 
 	"github.com/skpr/cli/internal/client"
+	"github.com/skpr/cli/internal/client/config"
 )
 
 // Command for console access.
@@ -18,6 +19,11 @@ type Command struct {
 
 // Run the command.
 func (cmd *Command) Run(ctx context.Context) error {
+	config, err := config.New()
+	if err != nil {
+		return err
+	}
+
 	ctx, client, err := client.New(ctx)
 	if err != nil {
 		return err
@@ -30,7 +36,7 @@ func (cmd *Command) Run(ctx context.Context) error {
 	if cluster.Endpoints == nil || cluster.Endpoints.Console == "" {
 		return fmt.Errorf("cluster does not have a console domain")
 	}
-	consoleURL := cluster.Endpoints.Console
+	consoleHost := cluster.Endpoints.Console
 
 	_, err = client.Environment().Get(ctx, &pb.EnvironmentGetRequest{
 		Name: cmd.Environment,
@@ -38,6 +44,8 @@ func (cmd *Command) Run(ctx context.Context) error {
 	if err != nil {
 		return fmt.Errorf("failed to get environment: %w", err)
 	}
+
+	consoleURL := fmt.Sprintf("https://%s/projects/%s/%s/metrics", consoleHost, config.Project, cmd.Environment)
 
 	if cmd.Print {
 		fmt.Println(consoleURL)
